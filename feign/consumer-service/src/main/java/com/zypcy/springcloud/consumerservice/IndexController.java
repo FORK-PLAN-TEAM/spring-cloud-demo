9e3e4b359897fdcd2d90d6b4e1ec6487c79eb23b
朱宇 <645906265@qq.com>
2018-12-10 16:03:30 +0800

    添加feign的示例项目

---
+++ feign/consumer-service/src/main/java/com/zypcy/springcloud/consumerservice/IndexController.java
@@ -0,0 +1,17 @@
+package com.zypcy.springcloud.consumerservice;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class IndexController {
+
+    @Autowired private HelloFeignService feignService;
+
+    @RequestMapping(value = "/hello" , method = RequestMethod.GET)
+    public String hello(String name){
+        return feignService.hello(name);
+    }
+}
