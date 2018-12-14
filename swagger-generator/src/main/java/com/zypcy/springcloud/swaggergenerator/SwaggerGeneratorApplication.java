9295660be787c30e5587490b44cca50658f6c092
朱宇 <645906265@qq.com>
2018-12-14 14:11:54 +0800

    添加项目swagger的api生成html文档

---
+++ swagger-generator/src/main/java/com/zypcy/springcloud/swaggergenerator/SwaggerGeneratorApplication.java
@@ -0,0 +1,14 @@
+package com.zypcy.springcloud.swaggergenerator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwaggerGeneratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SwaggerGeneratorApplication.class, args);
+	}
+
+}
+
