8f5e467b8db3a8c7b615966895a8a63cdb0c92f3
朱宇 <645906265@qq.com>
2019-09-17 16:10:08 +0800

    添加SpringBoot集成ELK收集日志

---
+++ logcollect/src/main/java/com/zypcy/elk/logcollect/LogcollectApplication.java
@@ -0,0 +1,30 @@
+package com.zypcy.elk.logcollect;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@SpringBootApplication
+public class LogcollectApplication {
+
+    Logger logger = LoggerFactory.getLogger(LogcollectApplication.class);
+
+    @GetMapping("log")
+    public void test(){
+        logger.debug("message debug");
+        logger.info("message info");
+        logger.warn("message warn");
+        logger.error("message error");
+        logger.info("测试初始一些日志吧！");
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(LogcollectApplication.class, args);
+    }
+
+}
