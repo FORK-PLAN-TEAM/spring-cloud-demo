22b9dfab879f54c7cd5e86f3b4d26bbe25bc71a4
china_zy <645906265@qq.com>
2020-06-19 11:30:46 +0800

    通过注解，让返回的实体数据有身份证号码的字段自动脱敏

---
+++ sensitive-serialize/src/main/java/com/zypcy/sensitiveserialize/SensitiveSerializeApplication.java
@@ -0,0 +1,13 @@
+package com.zypcy.sensitiveserialize;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SensitiveSerializeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SensitiveSerializeApplication.class, args);
+    }
+
+}
---
+++ sensitive-serialize/src/test/java/com/zypcy/sensitiveserialize/SensitiveSerializeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zypcy.sensitiveserialize;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SensitiveSerializeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
